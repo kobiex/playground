170c73c3ac21aadc2a172e94f7d009b134dbe2c8
Kobie Danquah <userkobie@gmail.com>
2025-06-30 06:26:10 +0000

    basics of goroutines

---
+++ concurrency/fork-join/bench_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"sync"
+	"testing"
+)
+
+func BenchmarkContextSwitch(b *testing.B) {
+	var wg sync.WaitGroup
+	begin := make(chan struct{})
+	c := make(chan struct{})
+	var token struct{}
+	sender := func() {
+		defer wg.Done()
+		<-begin
+		for b.Loop() {
+			c <- token
+		}
+	}
+	receiver := func() {
+		defer wg.Done()
+		<-begin
+		for b.Loop() {
+			<-c
+		}
+	}
+	wg.Add(2)
+	go sender()
+	go receiver()
+	b.StartTimer()
+	close(begin)
+	wg.Wait()
+}
